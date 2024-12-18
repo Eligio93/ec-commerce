1249cd30b54aadbfb4eaf84530e9fdf1e9a16ccc
Eligio93 <eligio.cristantielli@gmail.com>
2024-12-18 16:16:51 +1100

    defines user type interface

---
+++ interfaces/user.interface.ts
@@ -0,0 +1,21 @@
+import { Types } from "mongoose";
+
+export default interface UserInterface {
+    name: string;
+    lastName: string;
+    email: string;
+    password?: string;
+    profilePicture?: string;
+    orders?: Types.ObjectId[];
+    address?: {
+        country?: string;
+        city?: string;
+        zipCode?: string;
+        street?: {
+            address1?: string;
+            address2?: string;
+        };
+    };
+    isAdmin: boolean;
+    isRegistered: boolean;
+}
