50a8e3eb904ae48a50018c3ea37e4bb2c756b9c0
Eligio93 <eligio.cristantielli@gmail.com>
2024-12-20 11:30:53 +1100

    init component for mobile

---
+++ components/Header/Header.tsx
@@ -0,0 +1,64 @@
+"use client";
+import Navbar from "./Navbar";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Header() {
+  const [openMenu, setOpenMenu] = useState(true);
+  const [openCart, setOpenCart] = useState(false);
+  const [openUser, setOpenUser] = useState(false);
+
+  return (
+    <div className="header h-16 flex items-center p-3 text-orange-400 rounded-b-lg relative">
+      {openMenu && <Navbar />}
+      {/* Hamburger */}
+      <button onClick={() => setOpenMenu(!openMenu)} className="flex-1 justify-start">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-6 cursor-pointer"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      {/*Title or Logo*/}
+      <Link href="/" className="font-bold mx-auto text-3xl flex-1 text-center">
+        Satur
+      </Link>
+      <div className="flex items-center gap-4 flex-1 justify-end">
+        {/*Cart*/}
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 "
+          >
+            <path d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z" />
+          </svg>
+        </button>
+        {/*Profile*/}
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
