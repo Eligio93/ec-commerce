4f30b132a6761a3ae64f053795f56dcc6f2bbcad
Eligio93 <eligio.cristantielli@gmail.com>
2024-12-21 12:17:57 +1100

    defines menu navbar on large screens

--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
+// interface NavbarProps {
+//   openMenu: boolean;
+// }
 
-export default function Navbar() {
+export default function Navbar({ openMenu }: { openMenu: boolean }) {
   return (
-    <nav className="bg-white absolute top-16 left-0 border border-orange-300 w-full p-3">
-      <ul>
+    <nav
+      className={`bg-white ${
+        openMenu ? "block" : "hidden"
+      }    absolute top-16 left-0 border border-orange-300 w-full p-3 lg:flex lg:static lg:border-none lg:flex-1 lg:p-0`}
+    >
+      <ul className="lg:flex lg:justify-between lg:w-full items-center">
         <li>
           <Link href={"/"}>Home</Link>
         </li>
         <li>
-          <Link href={"/"}>Categories</Link>
+          <Link href={"/categories"}>Categories</Link>
         </li>
         <li>
-          <Link href={"/"}>All Products</Link>
+          <Link href={"/products"}>All Products</Link>
         </li>
       </ul>
     </nav>
