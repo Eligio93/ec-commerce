24551c952db93c5b04a87dbb9b643015dbc59abb
Eligio93 <eligio.cristantielli@gmail.com>
2024-12-28 01:06:24 +1100

    fill the carousel with elements

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,13 +1,33 @@
 "use client";
 import useEmblaCarousel from "embla-carousel-react";
+import menHeroImg from "@/public/menHero.jpg";
+import womanHeroImg from "@/public/womenHero.jpg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
   const [emblaRef] = useEmblaCarousel();
   return (
     <div className="embla overflow-hidden" ref={emblaRef}>
       <div className="embla__container flex">
-        <div className="embla__slide flex-[0_0_100%]">Slide 1</div>
-        <div className="embla__slide flex-[0_0_100%]">Slide 2</div>
+        <div className="embla__slide flex-[0_0_100%] relative sm:flex-[0_0_50%]">
+          <Image src={menHeroImg} alt="menHeroImg" priority />
+          <Link
+            className="absolute bottom-6 left-3 rounded-full bg-orange-400 px-3 py-1 font-bold"
+            href={"/products/men"}
+          >
+            Explore Men
+          </Link>
+        </div>
+        <div className="embla__slide flex-[0_0_100%] relative sm:flex-[0_0_50%]">
+          <Image src={womanHeroImg} alt="menHeroImg" priority />
+          <Link
+            className="absolute bottom-6 left-3 rounded-full bg-orange-400 px-3 py-1 font-bold"
+            href={"/products/men"}
+          >
+            Explore Women
+          </Link>
+        </div>
       </div>
     </div>
   );
