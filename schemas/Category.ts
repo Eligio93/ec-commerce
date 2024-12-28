d53ea7231e3e482523ebbfab71fcfd50d9800640
Eligio93 <eligio.cristantielli@gmail.com>
2024-12-28 17:09:29 +1100

    init schema

---
+++ schemas/Category.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+import CategoryInterface from "@/interfaces/category.interface";
+
+const CategorySchema = new Schema<CategoryInterface>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image:{type:String,required:true},
+  isFeatured: { type: Boolean, required: true },
+});
+
+const Category =
+  mongoose.models.Category ||
+  mongoose.model<CategoryInterface>("Category", CategorySchema);
+
+export default Category;
