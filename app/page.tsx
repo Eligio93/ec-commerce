cecc5fbf68fd51aadcd97a616e5c9f439ec7f716
Eligio93 <eligio.cristantielli@gmail.com>
2024-12-30 15:11:37 +1100

    import main Categories component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "@/components/Hero/Hero";
-
+import MainCategories from "@/components/MainCategories/MainCategories";
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col gap-5">
       <Hero />
+      <MainCategories />
     </div>
   );
 }
