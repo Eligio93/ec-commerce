1b849c18edc16971e99d7b7a207817ca3e6df0f2
Eligio93 <eligio.cristantielli@gmail.com>
2025-01-03 13:11:36 +1100

    set Loading  and adds types to states

---
+++ components/Home/MainCategories/MainCategories.tsx
@@ -0,0 +1,73 @@
+"use client";
+import CategoryInterface from "@/interfaces/category.interface";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function MainCategories() {
+  const [categories, setCategories] = useState<CategoryInterface[]>([]);
+  const [viewMore, setViewMore] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  //get featured categories
+  useEffect(() => {
+    const fetchFeaturedCategories = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_URL}/api/categories`
+        );
+        const data = await res.json();
+        const featuredCategories = data.filter(
+          (category: CategoryInterface) => category.isFeatured === true
+        );
+        setCategories(featuredCategories);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFeaturedCategories();
+  }, []);
+  if (loading) return <p className="text-center">Loading</p>;
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h2 className="text-center">Explore Main Products</h2>
+      {categories.length > 0 ? (
+        <div className="flex flex-wrap justify-center gap-5">
+          {categories.map((category: CategoryInterface, index: number) => (
+            <Link
+              href={`/products?category=${category.name}`}
+              key={category._id}
+              className={`
+              ${index > 3 && !viewMore ? "hidden" : "flex"}
+             flex-col gap-3 items-center flex-[0_0_100%] sm:flex  sm:p-2 sm:flex-[0_0_48%] md:flex-[0_0_28%] lg:flex-[0_0_20%] rounded-lg border hover:shadow-lg`}
+            >
+              <div className="relative w-full h-[100px] md:h-[150px]">
+                <Image
+                  src={category.image}
+                  alt={category.name}
+                  fill
+                  sizes="100%"
+                  className="object-cover rounded-lg"
+                  placeholder="blur"
+                  blurDataURL={category.image}
+                />
+              </div>
+              <p className="font-bold">{category.name}</p>
+            </Link>
+          ))}
+          <button
+            className="bg-orange-400 px-3 py-1 font-bold rounded-full sm:hidden"
+            onClick={() => setViewMore(!viewMore)}
+          >
+            {viewMore ? "View Less" : "View More"}
+          </button>
+        </div>
+      ) : (
+        <p className="text-center">There s no main products categories</p>
+      )}
+    </div>
+  );
+}
