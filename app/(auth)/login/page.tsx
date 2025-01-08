1a9020714ac1fa09e125ea07b67eec8b1f63453f
Eligio93 <eligio.cristantielli@gmail.com>
2025-01-08 13:26:48 +1100

    init login component and handle credentials login

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+  const [error, setError] = useState<string>();
+  const router = useRouter();
+
+  async function handleLogin(e: any) {
+    e.preventDefault();
+    setError(undefined);
+    const res = await signIn("credentials", {
+      redirect: false,
+      email: e.target.email.value,
+      password: e.target.password.value,
+    });
+    if (res?.error) {
+      setError("Invalid email or password");
+    }
+    if (res?.ok) {
+      router.back();
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin} className="flex flex-col">
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      {error && <p>{error}</p>}
+      <button type="submit">Login</button>
+    </form>
+  );
+}
