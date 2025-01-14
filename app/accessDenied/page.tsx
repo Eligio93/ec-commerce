c2ea10c3eca2a707d4e722ad30aff06998e39489
Eligio93 <eligio.cristantielli@gmail.com>
2025-01-14 20:50:25 +1100

    init page for access denied

---
+++ app/accessDenied/page.tsx
@@ -0,0 +1,23 @@
+import accessDeniedIcon from "@/public/accessDeniedIcon.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function AccessDenied() {
+  return (
+    <div className="flex flex-col  items-center h-[calc(100vh-64px)] gap-7 ">
+      <Image
+        className="max-h-[400px]"
+        src={accessDeniedIcon}
+        alt="accessDeniedIcon"
+      />
+      <h1 className="text-2xl font-bold ">Access Denied</h1>
+      <p>To access this page you must be an Admin</p>
+      <Link
+        className="rounded-full bg-orange-400 px-3 py-1 font-bold"
+        href="/dashboard"
+      >
+        Go to Dashboard
+      </Link>
+    </div>
+  );
+}
