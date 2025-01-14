fdb38992d36e10f6159f2469e8ae715c7cbabba7
Eligio93 <eligio.cristantielli@gmail.com>
2025-01-14 20:48:12 +1100

    removes id as string from interfaces

--- interfaces/category.interface.ts
+++ interfaces/category.interface.ts
@@ -1,5 +1,4 @@
 export default interface CategoryInterface {
-  _id: string;
   name: string;
   description: string;
   image: string;
--- interfaces/product.interface.ts
+++ interfaces/product.interface.ts
@@ -1,7 +1,6 @@
 import { Types } from "mongoose";
 
 export default interface ProductInterface {
-  _id: string;
   name: string;
   description: string;
   brand: string;
