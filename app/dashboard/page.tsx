adcbe2c253ae22291154247280ad2a0ed7ee380d
Eligio93 <eligio.cristantielli@gmail.com>
2025-01-17 15:13:51 +1100

    Hydrate User Interface

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,16 +2,17 @@ import UserInterface from "@/interfaces/user.interface";
 import { useEffect, useState } from "react";
 import DashboardProfile from "@/components/Dashboard/DashboardProfile";
 import DashboardAddress from "@/components/Dashboard/DashboardAddress";
+import { HydratedDocument } from "mongoose";
 
 type DashboardProps = {
   selectedField: string;
-  user: UserInterface;
+  user: HydratedDocument<UserInterface>;
 };
 
 export default function Dashboard({ selectedField, user }: DashboardProps) {
   if (selectedField === "Profile") {
     return (
-      <div>
+      <div className="flex flex-col gap-5">
         <DashboardProfile user={user} />
         <DashboardAddress user={user} />
       </div>
