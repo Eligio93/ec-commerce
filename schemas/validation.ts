e20e218fd847a1c6f4700396c72e5cd741a8e6b2
Eligio93 <eligio.cristantielli@gmail.com>
2025-01-21 19:59:09 +1100

    create editProfile validation schema

--- schemas/validation.ts
+++ schemas/validation.ts
@@ -38,3 +38,42 @@ export type AuthFormState =
       success?: boolean;
     }
   | undefined;
+
+export const EditProfileSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters" })
+    .max(15, { message: "Name must be at most 15 characters" })
+    .trim(),
+  lastName: z
+    .string()
+    .min(2, { message: "Last Name must be at least 2 characters" })
+    .max(30, { message: "Last Name must be at most 30 characters" })
+    .trim(),
+  email: z
+    .string()
+    .email({ message: "Please enter a valid email address" })
+    .trim(),
+  oldPassword: z
+    .string()
+    .min(8, { message: "Be at least 8 characters long" })
+    .optional(),
+
+  newPassword: z
+    .string()
+    .min(8, { message: "Be at least 8 characters long" })
+    .optional(),
+});
+export type ProfileFormState =
+  | {
+      errors?: {
+        name?: string[];
+        lastName?: string[];
+        email?: string[];
+        oldPassword?: string[];
+        newPassword?: string[];
+      };
+      message?: string;
+      success?: boolean;
+    }
+  | undefined;
