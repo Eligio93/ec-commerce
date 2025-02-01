11faf7d113f90d9f5252c314f96df70c9f61af74
Eligio93 <eligio.cristantielli@gmail.com>
2025-02-01 12:46:53 +1100

    init page to create a new product

---
+++ app/admin/newProduct/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import ProductForm from "@/components/Products/ProductForm";
+
+export default function NewProduct() {
+
+  return (
+    <div>
+      <h1>Create a new Product</h1>
+      <ProductForm />
+    </div>
+  );
+}
