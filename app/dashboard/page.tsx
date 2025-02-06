16ce6734708be09228cf93630259ed94a65e790c
Eligio93 <eligio.cristantielli@gmail.com>
2025-02-06 15:50:05 +1100

    adds profile and address components to the dashboard and fetch user

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,67 @@
+'use client'
 import UserInterface from "@/interfaces/user.interface";
 import { useEffect, useState } from "react";
 import DashboardProfile from "@/components/Dashboard/DashboardProfile";
 import DashboardAddress from "@/components/Dashboard/DashboardAddress";
 import { HydratedDocument } from "mongoose";
+import { useSession } from "next-auth/react";
+import dashboardIcon from "@/public/dashboardIcon.svg";
+import Image from "next/image";
 
 type DashboardProps = {
   selectedField: string;
   user: HydratedDocument<UserInterface>;
 };
 
-export default function Dashboard({ selectedField, user }: DashboardProps) {
-  if (selectedField === "Profile") {
-    return (
-      <div className="flex flex-col gap-5">
+export default function Dashboard() {
+  const [user, setUser] = useState<HydratedDocument<UserInterface> | undefined>(
+    undefined
+  );
+
+  const [openSidebar, setOpenSidebar] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [selectedOption, setSelectedOption] = useState<"Profile" | "Orders">(
+    "Profile"
+  );
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    async function fetchUser() {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_URL}/api/users/${session?.user.id}`
+        );
+        const data = await res.json();
+        if (res.ok) {
+          setErrorMessage(null);
+          setUser(data);
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error: any) {
+        console.log(error);
+        setErrorMessage(error);
+      }
+    }
+    if (status === "authenticated") {
+      fetchUser();
+    }
+  }, [session]);
+
+
+
+  return (
+    <div className="flex flex-col p-3 border border-orange-300 rounded-sm gap-20">
+      <div className="flex items-center">
+        <div className="size-6" onClick={() => setOpenSidebar(!openSidebar)}>
+          <Image src={dashboardIcon} alt="dashboardIcon" />
+        </div>
+        <h1 className="flex-1 text-center">Dashboard</h1>
+      </div>
+      {errorMessage && <p>{errorMessage}</p>}
+      {user && selectedOption === "Profile" && <div className="flex flex-col gap-5">
         <DashboardProfile user={user} />
         <DashboardAddress user={user} />
-      </div>
-    );
-  }
+      </div>}
+    </div>
+  );
 }
