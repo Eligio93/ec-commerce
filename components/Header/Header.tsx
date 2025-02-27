dcce14627a42672d0dcb7dfe0c7901508cc2121f
Eligio93 <eligio.cristantielli@gmail.com>
2025-02-27 12:32:00 +1100

    adds animation and functionality to close menu when click Outside

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Navbar from "./Navbar";
 import Link from "next/link";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
 import profileIcon from "@/public/profileIcon.svg";
@@ -9,7 +9,13 @@ import cartIcon from "@/public/cartIcon.svg";
 import hamburgerIcon from "@/public/hamburgerIcon.svg";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import { cartContext, CartProduct } from "@/config/CartProvider";
+import { cartContext } from "@/config/CartProvider";
+import Cart from "../Cart/Cart";
+import { AnimatePresence } from "motion/react";
+import { motion } from 'motion/react'
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+
 export default function Header() {
   const { cartProducts } = useContext(cartContext);
   const [openMenu, setOpenMenu] = useState(false);
@@ -17,10 +23,37 @@ export default function Header() {
   const [openUser, setOpenUser] = useState(false);
   const { data: session, status } = useSession();
   const location = usePathname();
+  const menuRef = useRef<HTMLDivElement>(null);
+  const cartRef = useRef<HTMLDivElement>(null);
+  const userRef = useRef<HTMLDivElement>(null);
   //when change location the menus get closed
   useEffect(() => {
     closeAllMenus();
   }, [location]);
+
+  //close menus when click outside of the menu
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      const cartIcon = document.querySelector('[data-cart-icon]');
+      const userIcon = document.querySelector('[data-user-icon]');
+
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setOpenMenu(false);
+      }
+      if (cartRef.current && !cartRef.current.contains(event.target as Node) &&
+        !cartIcon?.contains(event.target as Node)) {
+        setOpenCart(false);
+      }
+      if (userRef.current && !userRef.current.contains(event.target as Node) &&
+        !userIcon?.contains(event.target as Node)) {
+        setOpenUser(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  })
   //invoke this function everytime we open a new menu to close the rest of the menus
   function closeAllMenus() {
     setOpenMenu(false);
@@ -28,7 +61,7 @@ export default function Header() {
     setOpenUser(false);
   }
   return (
-    <div className="header h-16 flex items-center p-3 text-orange-400 rounded-b-lg relative">
+    <div className="header bg-orange-100 sticky h-16 flex items-center p-3 top-0 z-50">
       <div className="flex-1 justify-start lg:hidden ">
         {/* Hamburger */}
         <div
@@ -36,9 +69,9 @@ export default function Header() {
             closeAllMenus();
             setOpenMenu(!openMenu);
           }}
-          className="size-6 cursor-pointer"
+          className="size-6 cursor-pointer transition-opacity "
         >
-          <Image src={hamburgerIcon} alt="hamburgerIcon" />
+          {openMenu ? <XMarkIcon className="size-6 stroke-red-500" /> : <Image src={hamburgerIcon} alt="hamburgerIcon" />}
         </div>
       </div>
 
@@ -51,27 +84,32 @@ export default function Header() {
       </Link>
 
       {/*NavBar*/}
-      <Navbar openMenu={openMenu} />
+      <AnimatePresence initial={false}>
+        <Navbar openMenu={openMenu} reference={menuRef as React.RefObject<HTMLDivElement>} />
+      </AnimatePresence>
+
+
 
       <div className="flex items-center gap-4 flex-1 justify-end sm:gap-5 md:gap-6 lg:gap-7 ">
         {/*Cart*/}
         <div
-          className="size-6 text-orange-400 cursor-pointer lg:size-8"
+          className="size-6 text-orange-400 cursor-pointer lg:size-8 relative"
+          data-cart-icon
           onClick={(e) => {
             closeAllMenus();
             setOpenCart(!openCart);
           }}
         >
           <Image src={cartIcon} alt="cartIcon" />
+          {cartProducts.length > 0 && <div className="absolute h-3 w-3 bg-orange-800 rounded-full top-0.5 right-0 animate-bounce">
+          </div>}
         </div>
-        {openCart && (
-          <div className="absolute z-10 right-0 top-full bg-white border w-3/4 h-[calc(100vh-64px)]">
-            {cartProducts.length <= 0 ? <p>The cart is empty</p> : cartProducts.map((product: CartProduct) => <p key={product.product.name}>{product.product.name}</p>)}
-          </div>
-        )}
+        <AnimatePresence initial={false}>
+          {openCart && <Cart reference={cartRef as React.RefObject<HTMLDivElement>} />}</AnimatePresence>
         {/*Profile*/}
         <div
           className="size-6 text-orange-400 cursor-pointer relative lg:size-8"
+          data-user-icon
           onClick={(e) => {
             closeAllMenus();
             setOpenUser(!openUser);
@@ -89,35 +127,45 @@ export default function Header() {
             <Image src={profileIcon} alt="profileIcon" />
           )}
         </div>
-        {openUser && (
-          <div className="absolute z-10 right-0 top-full bg-white border">
-            <nav>
-              {status === "authenticated" && (
-                <ul className="flex flex-col">
-                  <li>
-                    <Link href={"/dashboard"}>Dashboard</Link>
-                  </li>
-                  {session?.user.isAdmin && (
+        <AnimatePresence initial={false}>
+          {openUser && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0 }}
+              className="absolute z-10 right-0 top-full bg-white border"
+              ref={userRef}
+
+            >
+              <nav >
+                {status === "authenticated" && (
+                  <ul className="flex flex-col">
                     <li>
-                      <Link href={"/admin"}>Admin</Link>
+                      <Link href={"/dashboard"}>Dashboard</Link>
                     </li>
-                  )}
-                  <li>
-                    <button onClick={async () => await signOut()}>
-                      Logout
-                    </button>
-                  </li>
-                </ul>
-              )}
-              {status === "unauthenticated" && (
-                <ul className="flex flex-col">
-                  <Link href={"/login"}>Login</Link>
-                  <Link href={"/register"}>Create a new Account</Link>
-                </ul>
-              )}
-            </nav>
-          </div>
-        )}
+                    {session?.user.isAdmin && (
+                      <li>
+                        <Link href={"/admin"}>Admin</Link>
+                      </li>
+                    )}
+                    <li>
+                      <button onClick={async () => await signOut()}>
+                        Logout
+                      </button>
+                    </li>
+                  </ul>
+                )}
+                {status === "unauthenticated" && (
+                  <ul className="flex flex-col">
+                    <Link href={"/login"}>Login</Link>
+                    <Link href={"/register"}>Create a new Account</Link>
+                  </ul>
+                )}
+              </nav>
+            </motion.div>
+          )}
+        </AnimatePresence>
+
       </div>
     </div>
   );
