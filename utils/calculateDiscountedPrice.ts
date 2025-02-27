3618c1a5fbd812b07381d6cf918e52699a19c4cc
Eligio93 <eligio.cristantielli@gmail.com>
2025-02-27 12:32:28 +1100

    adds quantity to calculate the final price in cart

--- utils/calculateDiscountedPrice.ts
+++ utils/calculateDiscountedPrice.ts
@@ -1,3 +1,7 @@
-export default function calculateDiscountedPrice(price: number, discount: number) {
-    return (price - price * discount / 100).toFixed(2);
+export default function calculateDiscountedPrice(
+  price: number,
+  discount: number = 0,
+  quantity: number = 1
+) {
+  return ((price - (price * discount) / 100) * quantity).toFixed(2);
 }
