5aa8370e820beaff48cf9cf1ddc6588e92e8ad12
Eligio93 <eligio.cristantielli@gmail.com>
2025-02-27 13:00:23 +1100

    add reference from header

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -5,7 +5,7 @@ import CartProductListing from "./CartProductListing"
 import { motion } from 'motion/react'
 import { ShoppingBagIcon } from "@heroicons/react/24/outline"
 
-export default function Cart() {
+export default function Cart({ reference }: { reference: React.RefObject<HTMLDivElement> }) {
     const { cartProducts } = useContext(cartContext)
 
 
@@ -13,11 +13,13 @@ export default function Cart() {
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0 }}
-        className="absolute shadow-lg rounded-lg z-10 right-0 top-full bg-white border w-3/4 lg:w-1/4 h-[calc(100vh-64px)] overflow-scroll   [&::-webkit-scrollbar]:w-2
+        className="absolute shadow-lg rounded-lg z-10 right-0 top-full bg-white border w-3/4 max-w-[350px] lg:w-1/4 h-[calc(100vh-64px)] overflow-scroll   [&::-webkit-scrollbar]:w-2
   [&::-webkit-scrollbar-track]:rounded-full
   [&::-webkit-scrollbar-track]:bg-gray-100
   [&::-webkit-scrollbar-thumb]:rounded-full
-  [&::-webkit-scrollbar-thumb]:bg-orange-800">
+  [&::-webkit-scrollbar-thumb]:bg-orange-800"
+        ref={reference}
+    >
         {cartProducts.length <= 0 ?
             <div className="flex flex-col justify-center items-center h-full gap-5 ">
                 <ShoppingBagIcon className="size-10 stroke-slate-500" />
