79e0093974e6dc51a2df16e4120492b1b6f8a5f3
Eligio93 <eligio.cristantielli@gmail.com>
2025-03-01 11:36:53 +1100

    adds cart checkout section

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -4,6 +4,8 @@ import { CartProduct } from "@/config/CartProvider"
 import CartProductListing from "./CartProductListing"
 import { motion } from 'motion/react'
 import { ShoppingBagIcon } from "@heroicons/react/24/outline"
+import Link from "next/link"
+import EmptyCartBtn from "./EmptyCartBtn"
 
 export default function Cart({ reference }: { reference: React.RefObject<HTMLDivElement> }) {
     const { cartProducts } = useContext(cartContext)
@@ -13,11 +15,11 @@ export default function Cart({ reference }: { reference: React.RefObject<HTMLDiv
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0 }}
-        className="absolute shadow-lg rounded-lg z-10 right-0 top-full bg-white border w-3/4 max-w-[350px] lg:w-1/4 h-[calc(100vh-64px)] overflow-scroll   [&::-webkit-scrollbar]:w-2
+        className={`absolute shadow-lg rounded-lg z-10 right-0 top-full bg-white  border-2 border-orange-800 w-3/4 max-w-[350px] lg:w-1/4 ${cartProducts.length <= 0 ? 'h-[calc(100vh-64px)]' : 'max-h-[calc(100vh-64px)]'}  overflow-y-auto  [&::-webkit-scrollbar]:w-1
   [&::-webkit-scrollbar-track]:rounded-full
   [&::-webkit-scrollbar-track]:bg-gray-100
   [&::-webkit-scrollbar-thumb]:rounded-full
-  [&::-webkit-scrollbar-thumb]:bg-orange-800"
+  [&::-webkit-scrollbar-thumb]:bg-orange-800`}
         ref={reference}
     >
         {cartProducts.length <= 0 ?
@@ -26,9 +28,19 @@ export default function Cart({ reference }: { reference: React.RefObject<HTMLDiv
                 <p className="text-slate-500">The cart is empty</p>
             </div>
             :
-            <div className="flex flex-col gap-4 p-2 ">
-                {cartProducts.map((product: CartProduct) => <CartProductListing key={product.product._id.toString()} cartItem={product} />)}
-            </div>
+            <>
+                <div className="flex flex-col gap-4 p-2 ">
+                    {cartProducts.map((product: CartProduct) => <CartProductListing key={product.product._id.toString()} cartItem={product} />)}
+
+                </div>
+                <div className="bg-orange-200 text-white flex flex-col gap-2 p-2 sticky bottom-0 rounded-t-lg ">
+                    <p className="font-bold text-black">Total:</p>
+                    <hr className="border-orange-800" />
+                    <p className="font-bold text-black text-lg text-end">${cartProducts.reduce((acc: number, product: CartProduct) => acc + product.product.price * product.quantity, 0).toFixed(2)}</p>
+                    <Link className="text-center transition-colors duration-200 text-sm sm:text-base py-1 lg:py-2 bg-green-500 rounded-lg font-bold hover:bg-green-700 hover:text-white" href={'#'}>Checkout</Link>
+                    <EmptyCartBtn />
+                </div>
+            </>
         }
 
     </motion.div>
