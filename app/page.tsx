1aff09250de7e3006424fff3bfcf49ac3583dc7f
Eligio93 <eligio.cristantielli@gmail.com>
2025-03-13 16:40:05 +1100

    import New Arrivals component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Hero from "@/components/Hero/Hero";
-import MainCategories from "@/components/MainCategories/MainCategories";
+import Hero from "@/components/Home/Hero/Hero";
+import MainCategories from "@/components/Home/MainCategories/MainCategories";
+import NewArrivals from "@/components/Home/NewArrivals/NewArrivals";
 export default function Home() {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-8">
       <Hero />
       <MainCategories />
+      <NewArrivals />
     </div>
   );
 }
