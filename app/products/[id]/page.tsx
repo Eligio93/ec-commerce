464d6881d61af893eef610b5cd3e436ad7f547b0
Eligio93 <eligio.cristantielli@gmail.com>
2025-03-18 01:00:43 +1100

    adds product info and style the layout of the page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -3,7 +3,12 @@ import Product from "@/schemas/Product";
 import ProductInterface from "@/interfaces/product.interface";
 import { HydratedDocument } from "mongoose";
 import "@/schemas/Category"
-import CategoryInterface from "@/interfaces/category.interface";
+import ProductImageCarousel from "@/components/Products/ProductImageCarousel";
+import Link from "next/link";
+import AddToCartController from "@/components/Products/AddToCartController";
+import calculateDiscountedPrice from "@/utils/calculateDiscountedPrice";
+import InfoProductListing from "@/components/Products/InfoProductListing";
+
 
 
 
@@ -15,15 +20,41 @@ export default async function ProductPage({
 }) {
 
     const id = (await params).id
+    //aggiornare le views del prodotto
     await connectDB();
     const product: HydratedDocument<ProductInterface> | null = await Product.findOne({ _id: id }).populate("category")
     console.log(product)
     if (product) {
         return (
-            <div>
-                <h1>{product.name}</h1>
-                <p>{product.description}</p>
-            </div>
+            <div className="flex flex-col gap-5 p-5">
+                <div className="flex flex-col gap-3 px-3 md:flex-row md:justify-evenly md:gap-5 md:items-center">
+                    {/*Carousel for Product Picture*/}
+                    <div className="flex flex-col gap-2">
+                        <ProductImageCarousel images={product.images} />
+                    </div>
+                    <div className="flex flex-col gap-3 md:justify-center lg:max-w-[600px]">
+                        {/*DIV for Product Title HEader*/}
+                        <div className="flex flex-col">
+                            <p className="text-orange-800 text-sm">{product.brand}</p>
+                            <h1 className="font-bold text-2xl lg:text-3xl">{product.name}</h1>
+                            <Link href={`/products?category=${product.category.name}`} className="font-bold text-xs hover:underline text-orange-500">{product.category.name}</Link>
+                        </div>
+                       
+                        {/*Div for product Price and Cart CTA*/}
+                        <div className="flex flex-col gap-4 bg-white p-3 rounded-lg">
+                        <p className="bg-white hidden lg:block leading-7 ">{product.description}</p>
+                            <div className="flex flex-wrap gap-2 items-center">
+                                {product.discount ? <p className="line-through text-gray-400">${product.price}</p> : <p className="font-bold text-xl">${product.price}</p>}
+                                {product.discount ? <p className="text-orange-400 font-bold text-l">-{product.discount}%</p> : null}
+                                {product.discount ? <p className="font-bold text-xl">${calculateDiscountedPrice(product.price, product.discount)}</p> : null}
+                            </div>
+                            <AddToCartController {...JSON.parse(JSON.stringify(product))} />
+                           
+                        </div>
+
+                    </div>
+                </div>
+                <InfoProductListing description={product.description} /></div>
         )
     } else
         return (
