615e8ebf5f890f8db2d1e207f20b319baa342499
Eligio93 <eligio.cristantielli@gmail.com>
2025-03-26 15:32:39 +1100

    improves structure and styling

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,21 +1,29 @@
 import Link from "next/link";
+import Image from "next/image";
+import { ClipboardDocumentListIcon, DocumentPlusIcon, FolderPlusIcon } from "@heroicons/react/24/outline";
 
 export default function AdminLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    return <div className="flex gap-5 border rounded-lg">
-        <div className="flex flex-col gap-5 bg-slate-50 p-3 rounded-lg h-[calc(100vh-66px)]">
-            <h2>Admin</h2>
-            <ul className="flex flex-col gap-2">
-                <Link href="/admin">Inventory</Link>
-                <Link href="admin/newProduct">Create New Product</Link>
-                <Link href="admin/newCategory">Create New Category</Link>
+    return <div className="flex rounded-lg sm:gap-2">
+        <div className="flex flex-col gap-5 bg-slate-50 p-2 rounded-lg min-h-[calc(100vh-66px)]">
+            <h2 className="text-center">ADMIN</h2>
+            <ul className="flex flex-col gap-3 justify-center">
+                <Link className="flex flex-col gap-2 items-center" href="/admin">
+                    <ClipboardDocumentListIcon className="size-7" />
+                    <p className='text-xs'>Inventory</p>
+                </Link>
+                <Link className="flex flex-col gap-2 items-center" href="/admin/newProduct">
+                    <DocumentPlusIcon className="size-7" />
+                    <p className='text-xs text-center'>New Product</p>
+                </Link>
+                <Link className="flex flex-col gap-2 items-center" href="/admin/newCategory">
+                    <FolderPlusIcon className="size-7" />
+                    <p className='text-xs text-center'>New Category</p>
+                </Link>
             </ul>
-
-
-
         </div>
         {children}
     </div>;
