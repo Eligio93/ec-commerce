ebe877e6723e6994a7a724ae813686b948ab4ed1
Eligio93 <eligio.cristantielli@gmail.com>
2025-03-30 00:43:30 +1100

    implement search bar and listing inventory

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,15 +1,71 @@
+'use client'
+
 import Link from "next/link";
-//show all products and implement search to find them 
-// create newProduct and newCategory Link
-// give the chance to edit every product and category
+import { HydratedDocument, set } from "mongoose";
+import ProductInterface from "@/interfaces/product.interface";
+import InventoryProductCard from "@/components/Products/InventoryProductCard";
+import { useState, useEffect } from "react";
+
+
 
 export default function Admin() {
-  return (
-    <div className="flex flex-col gap-5">
+  const [query, setQuery] = useState("")
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [products, setProducts] = useState<HydratedDocument<ProductInterface>[] | null>(null)
+  const [filteredProducts, setFilteredProducts] = useState<HydratedDocument<ProductInterface>[]>([])
 
-      <h1>Admin</h1>
-      <Link href="admin/newProduct">Create New Product</Link>
-      <Link href="admin/newCategory">Create New Category</Link>
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products`);
+        console.log(response)
+        const products: HydratedDocument<ProductInterface>[] = await response.json();
+        setProducts(products);
+      } catch (err) {
+        console.error("Errorin fetching products:", err);
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    getProducts();
+  }, [])
+
+  useEffect(() => {
+    if (products) {
+      if (query.trim() === "") {
+        setFilteredProducts(products)
+      } else {
+        const filteredArray = products.filter((product) => product.name.toLowerCase().includes(query.toLowerCase().trim()));
+        setFilteredProducts(filteredArray)
+      }
+    }
+  }, [query, products])
+
+  console.log(filteredProducts)
+  return (
+    <div className="w-full">
+      <h1>Inventory</h1>
+      {isLoading ? (<p>Loading...</p>) : (products ? (
+        <div className="flex flex-col gap-5 p-2">
+          <div className="w-full">
+            <input className="w-full rounded-lg p-2" type="text" placeholder="Search for Items" value={query} onChange={(e) => setQuery(e.target.value)} />
+          </div>
+          {
+            filteredProducts.length > 0 ? (
+              filteredProducts.map((product) => <InventoryProductCard key={product.name} product={product} />)
+            ) : (
+              <div>
+                <p>No products found</p>
+              </div>
+            )
+          }
+
+        </div>
+      ) : (
+        <p>No products</p>
+      ))}
+
     </div>
   );
 }
