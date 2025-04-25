72fd61eeb318a75911b437e94c5bb5ddc6f31341
Eligio93 <eligio.cristantielli@gmail.com>
2025-04-25 23:09:33 +1000

    import main brand section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Home/Hero/Hero";
 import MainCategories from "@/components/Home/MainCategories/MainCategories";
 import NewArrivals from "@/components/Home/NewArrivals/NewArrivals";
+import MainBrands from "@/components/Home/MainBrands/MainBrands";
 export default function Home() {
   return (
     <div className="flex flex-col gap-8">
       <Hero />
       <MainCategories />
       <NewArrivals />
+      <MainBrands />
     </div>
   );
 }
