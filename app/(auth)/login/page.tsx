10744e41c36a200391f31a77151656e59ebc2e86
Eligio Cristantielli <eligio.cristantielli@gmail.com>
2025-08-08 17:24:59 +0200

    complete style login form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,6 +3,9 @@ import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
+import Image from "next/image";
+import googleIcon from "@/public/googleIcon.svg";
+import loginIcon from "@/public/loginIcon.svg";
 
 export default function Login() {
   const [error, setError] = useState<string>();
@@ -26,26 +29,38 @@ export default function Login() {
   }
 
   return (
-    <div>
-      <form onSubmit={handleLogin} className="flex flex-col">
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
-        {error && <p>{error}</p>}
-        <button type="submit">Login</button>
-        <p>Or</p>
-      </form>
-      <button
-        type="button"
-        onClick={async () => {
-          await signIn("google", {
-            callbackUrl: searchParams.get("callbackUrl") || "/",
-          });
-        }}
+    <div className="p-2">
+      <form
+        onSubmit={handleLogin}
+        className="mx-auto flex max-w-lg flex-col gap-4 rounded-lg border border-orange-800 bg-white px-2 py-4 sm:p-6 md:max-w-xl lg:max-w-2xl"
       >
-        SIgnIn With Google
-      </button>
+        <div className="mb-3 flex flex-col gap-2 text-sm [&>input]:rounded-md [&>input]:border [&>input]:border-black [&>input]:p-1 md:text-base">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+          {error && <p>{error}</p>}
+        </div>
+        <button
+          type="submit"
+          className="flex items-center justify-center gap-2 rounded-full border py-1 text-sm font-bold transition-colors duration-500 hover:bg-orange-300 md:text-base"
+        >
+          <Image src={loginIcon} alt="LoginIcon" className="size-6" />
+          <p> Login </p>
+        </button>
+        <button
+          type="button"
+          className="flex items-center justify-center gap-2 rounded-full border py-1 font-bold transition-colors duration-500 hover:bg-orange-300 md:text-base"
+          onClick={async () => {
+            await signIn("google", {
+              callbackUrl: searchParams.get("callbackUrl") || "/",
+            });
+          }}
+        >
+          <Image src={googleIcon} alt="googleIcon" className="size-6" />
+          <p className="text-sm"> Sign In With Google</p>
+        </button>
+      </form>
     </div>
   );
 }
