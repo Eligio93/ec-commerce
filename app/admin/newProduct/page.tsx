415f002bc20049d016acacb64dfe11dc63d3f6eb
Eligio Cristantielli <eligio.cristantielli@gmail.com>
2025-08-11 15:52:28 +0200

    adds style and removes h1

--- app/admin/newProduct/page.tsx
+++ app/admin/newProduct/page.tsx
@@ -1,11 +1,8 @@
-"use client";
 import ProductForm from "@/components/Products/ProductForm";
 
 export default function NewProduct() {
-
   return (
-    <div>
-      <h1>Create a new Product</h1>
+    <div className="flex w-full flex-col gap-5">
       <ProductForm />
     </div>
   );
