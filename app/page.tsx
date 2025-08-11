dae4c5a053b091463145cef5f9d83472e2f1edbf
Eligio Cristantielli <eligio.cristantielli@gmail.com>
2025-08-11 14:01:10 +0200

    import Footer component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Home/Hero/Hero";
 import MainCategories from "@/components/Home/MainCategories/MainCategories";
 import NewArrivals from "@/components/Home/NewArrivals/NewArrivals";
 import MainBrands from "@/components/Home/MainBrands/MainBrands";
+import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <div className="flex flex-col gap-8 p-1">
@@ -9,6 +10,7 @@ export default function Home() {
       <MainCategories />
       <NewArrivals />
       <MainBrands />
+      <Footer />
     </div>
   );
 }
